a885c18f392ef40c718f0317c6f4f383361dd6b6
Kiran <kiran.s7903@gmail.com>
2022-07-07 21:13:14 +0530

    pushing first commit

---
+++ src/test/java/automate/Test.java
@@ -0,0 +1,20 @@
+package automate;
+
+public class Test {
+
+	
+
+		public static void main(String [] args)
+		{
+		     String s="india";
+		     String rev="";
+
+		for(int i=s.length()-1;i>=0;i--)
+		{
+		rev=rev+s.charAt(i);
+		}
+		System.out.print(rev);
+		}
+		}
+		     
+
